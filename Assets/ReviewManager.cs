1707a6b2c95f69c35c61a2c57d34939efd927044
stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>
2021-02-11 19:43:56 -0600

    initial project commit: starting project fundamentals with review and learning modes

---
+++ Assets/ReviewManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReviewManager : MonoBehaviour
+{
+
+    public GameObject ReviewButtons;
+    private string correctName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Instantiate(ReviewButtons, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(GameObject.Find("Name"));
+    }
+}
---
+++ Assets/play.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class play : MonoBehaviour
+{
+
+    public List<GameObject> foods;
+    public int index = 0;
+    private int maxIndex;
+    private GameObject currentFood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentFood = GameObject.Instantiate(foods[0], transform);
+        maxIndex = foods.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            index--;
+            if(index < 0)
+            {
+                index = maxIndex - 1;
+            }
+
+            Destroy(currentFood);
+            currentFood = GameObject.Instantiate(foods[index], transform);
+
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            index++;
+            if (index >= maxIndex)
+            {
+                index = 0;
+            }
+
+            Destroy(currentFood);
+            currentFood = GameObject.Instantiate(foods[index], transform);
+        }
+    }
+}
