0b76f9329c744690940e76641816a4688d87fa81
Michael McQuade <michael@giraffesyo.io>
2021-03-15 02:50:31 -0500

    Finish splitting review mode from learn mode into its own scene
    Co-authored-by: stwalsh4118 <40175938+stwalsh4118@users.noreply.github.com>

--- Assets/Scripts/LearnManager.cs
+++ Assets/Scripts/LearnManager.cs
@@ -3,19 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
-// public enum GameModes
-// {
-//     learn,
-//     review
-// }
-
 public class LearnManager : MonoBehaviour
 {
 
     public List<GameObject> objects;
     public List<string> objectNames;
-    // public List<GameObject> buttonTexts;
+
 
     public GameObject Instructions;
     public int index = 0;
@@ -23,11 +16,9 @@ public class LearnManager : MonoBehaviour
     public int maxIndex;
     private GameObject currentFood;
 
-    // public GameModes GameMode = GameModes.learn;
 
     // TODO: If we setup player preferences we can load this from there
     private bool shouldShowSwipeInstructions = false;
-    private int numberOfReviewModes = System.Enum.GetValues(typeof(ReviewModes)).Cast<int>().Max();
 
     void Start()
     {
@@ -83,9 +74,6 @@ public class LearnManager : MonoBehaviour
 
     void Update()
     {
-
-        // if (GameMode == GameModes.learn)
-        // {
         if (Input.GetKeyDown(KeyCode.A))
         {
             AdvanceLearnItems(backwards: true);
@@ -94,133 +82,6 @@ public class LearnManager : MonoBehaviour
         {
             AdvanceLearnItems();
         }
-        // }
-        // else if (GameMode == GameModes.review)
-        // {
-        //     // override to skip the question
-        //     if (Input.GetKeyDown(KeyCode.F))
-        //     {
-        //         NextReviewQuestion();
-        //     }
-
-        //     if (GameObject.Find("Name"))
-        //     {
-        //         currentName = currentFood.name.Replace("(Clone)", "");
-        //         Destroy(GameObject.Find("Name"));
-
-        //     }
-
-        //     //GameObject.Find("Object1")
-        // }
     }
-    // void SetGameMode(GameModes mode)
-    // {
-    //     GameMode = mode;
-    //     if (mode == GameModes.learn)
-    //     {
-    //         ReviewButtons.SetActive(false);
-    //         if (shouldShowSwipeInstructions)
-    //         {
-    //             Instructions.SetActive(true);
-    //         }
-    //         Destroy(currentFood);
-    //         currentFood = GameObject.Instantiate(objects[0], transform);
-    //     }
-    //     else if (mode == GameModes.review)
-    //     {
-    //         ReviewButtons.SetActive(true);
-    //         Instructions.SetActive(false);
-    //         reviewObjectList = new List<GameObject>(objects);
-    //         NextReviewQuestion();
-    //     }
-    // }
 
-    // public void CheckAnswer(GameObject buttonText)
-    // {
-    //     if (currentName == buttonText.GetComponent<TextMeshProUGUI>().text)
-    //     {
-    //         // TODO: they got it right, send message about their score for this item
-    //         NextReviewQuestion();
-    //     }
-    //     else
-    //     {
-    //         // FIXME: they got it wrong, show it agin later, show the right answer 
-    //         //TODO: possible todo: move it back one bucket
-    //     }
-    // }
-
-    // public void NextReviewQuestion()
-    // {
-    //     // get a random mode to switch to 
-    //     ReviewModes randomMode = (ReviewModes)Random.Range(0, numberOfReviewModes + 1);
-    //     if (randomMode == ReviewModes.Speech)
-    //     {
-    //         SpeechContainer.SetActive(true);
-    //         ThreeWordsContainer.SetActive(false);
-    //         ThreeObjectsContainer.SetActive(false);
-    //     }
-    //     else if (randomMode == ReviewModes.ThreeWords)
-    //     {
-    //         SpeechContainer.SetActive(false);
-    //         ThreeWordsContainer.SetActive(true);
-    //         ThreeObjectsContainer.SetActive(false);
-    //     }
-    //     else if (randomMode == ReviewModes.ThreeObjects)
-    //     {
-    //         SpeechContainer.SetActive(false);
-    //         ThreeWordsContainer.SetActive(false);
-    //         ThreeObjectsContainer.SetActive(true);
-    //     }
-
-    //     if (reviewObjectList.Count <= 0)
-    //     {
-    //         // FIXME: Return them to main menu, not learn mode
-    //         SetGameMode(GameModes.learn);
-    //         return;
-    //     }
-
-    //     int randomObject = Random.Range(0, reviewObjectList.Count);
-
-    //     Destroy(currentFood);
-    //     currentFood = GameObject.Instantiate(reviewObjectList[randomObject], transform);
-    //     reviewObjectList.RemoveAt(randomObject);
-    //     StartCoroutine(InitReviewButtons());
-    // }
-
-
-
-    // IEnumerator InitReviewButtons()
-    // {
-    //     yield return new WaitForEndOfFrame();
-    //     // get the name of the fruit from the cloned prefab
-    //     currentName = currentFood.name.Replace("(Clone)", "");
-    //     // pick a random number which will be used to put the correct answer on the button
-    //     int randomIndex = Random.Range(0, buttonTexts.Count);
-    //     // create a list of ALL possible objects in user's review queue, store it in temp
-    //     List<string> temp = new List<string>(objectNames);
-    //     // remove the correct answer from the list
-    //     temp.Remove(currentName);
-    //     // put three incorrect answers on the buttons
-    //     for (int i = 0; i < 3; i++)
-    //     {
-    //         string tempName = temp[Random.Range(0, temp.Count - 1)];
-    //         temp.Remove(tempName);
-    //         buttonTexts[i].GetComponent<TextMeshProUGUI>().text = tempName;
-    //     }
-    //     // put the correct answer on the previously selected button, overwriting one of the incorrect options
-    //     buttonTexts[randomIndex].GetComponent<TextMeshProUGUI>().text = currentName;
-
-    // }
-
-    // public void SwapMode()
-    // {
-    //     if (GameMode == GameModes.learn)
-    //     {
-    //         SetGameMode(GameModes.review);
-    //     }
-    //     else if (GameMode == GameModes.review)
-    //     {
-    //         SetGameMode(GameModes.learn);
-    //     }
-    // }
 }
--- Assets/Scripts/ReviewManager.cs
+++ Assets/Scripts/ReviewManager.cs
@@ -1,26 +1,143 @@
-﻿using System.Collections;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+public enum ReviewModes
+{
+    ThreeObjects,
+    ThreeWords,
+    Speech
+}
 
 public class ReviewManager : MonoBehaviour
 {
 
-    public GameObject ReviewButtons;
-    private string correctName;
-    // Start is called before the first frame update
+    public List<GameObject> objects;
+    public List<string> objectNames;
+    public List<TextMeshProUGUI> buttonTexts;
+    public int index = 0;
+    private string currentName;
+    public int maxIndex;
+    private GameObject currentFood;
+    [SerializeField]
+    private GameObject threeObjectsContainer;
+    [SerializeField]
+    private GameObject threeWordsContainer;
+    [SerializeField]
+    private GameObject speechContainer;
+
+    // TODO: If we setup player preferences we can load this from there
+    private int numberOfReviewModes = System.Enum.GetValues(typeof(ReviewModes)).Cast<int>().Max();
+
+    private List<GameObject> reivewQueue;
+
+    public SceneLoader sceneLoader;
+
     void Start()
     {
-        GameObject.Instantiate(ReviewButtons, transform);
+        // ReviewButtons.SetActive(false);
+        currentFood = GameObject.Instantiate(objects[0], transform);
+        sceneLoader = GetComponent<SceneLoader>();
+        maxIndex = objects.Count;
+        reivewQueue = new List<GameObject>(objects);
+        NextReviewQuestion();
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
-        Destroy(GameObject.Find("Name"));
+        // override to skip the question
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            NextReviewQuestion();
+        }
+
+        if (GameObject.Find("Name"))
+        {
+            currentName = currentFood.name.Replace("(Clone)", "");
+            Destroy(GameObject.Find("Name"));
+
+        }
     }
 
-    void UpdateButtonNames()
+
+    public void CheckAnswer(GameObject buttonText)
     {
+        if (currentName == buttonText.GetComponent<TextMeshProUGUI>().text)
+        {
+            // TODO: they got it right, send message about their score for this item
+            NextReviewQuestion();
+        }
+        else
+        {
+            // FIXME: they got it wrong, show it agin later, show the right answer 
+            //TODO: possible todo: move it back one bucket
+        }
+    }
+
+    public void NextReviewQuestion()
+    {
+        // get a random mode to switch to 
+        ReviewModes randomMode = (ReviewModes)Random.Range(0, numberOfReviewModes + 1);
+        if (randomMode == ReviewModes.Speech)
+        {
+            speechContainer.SetActive(true);
+            threeWordsContainer.SetActive(false);
+            threeObjectsContainer.SetActive(false);
+        }
+        else if (randomMode == ReviewModes.ThreeWords)
+        {
+            speechContainer.SetActive(false);
+            threeWordsContainer.SetActive(true);
+            threeObjectsContainer.SetActive(false);
+        }
+        else if (randomMode == ReviewModes.ThreeObjects)
+        {
+            speechContainer.SetActive(false);
+            threeWordsContainer.SetActive(false);
+            threeObjectsContainer.SetActive(true);
+        }
+
+        if (reivewQueue.Count <= 0)
+        {
+            sceneLoader.LoadMainMenu();
+            return;
+        }
+
+        int randomObject = Random.Range(0, reivewQueue.Count);
+
+        Destroy(currentFood);
+        currentFood = GameObject.Instantiate(reivewQueue[randomObject], transform);
+        reivewQueue.RemoveAt(randomObject);
+        StartCoroutine(InitReviewButtons());
+    }
+
+
+
+    IEnumerator InitReviewButtons()
+    {
+        yield return new WaitForEndOfFrame();
+        // get the name of the fruit from the cloned prefab
+        currentName = currentFood.name.Replace("(Clone)", "");
+        // pick a random number which will be used to put the correct answer on the button
+        int randomIndex = Random.Range(0, buttonTexts.Count);
+        // create a list of ALL possible objects in user's review queue, store it in temp
+        List<string> temp = new List<string>(objectNames);
+        // remove the correct answer from the list
+        temp.Remove(currentName);
+        // put three incorrect answers on the buttons
+        for (int i = 0; i < 3; i++)
+        {
+            string tempName = temp[Random.Range(0, temp.Count - 1)];
+            temp.Remove(tempName);
+            buttonTexts[i].text = tempName;
+        }
+        // put the correct answer on the previously selected button, overwriting one of the incorrect options
+        buttonTexts[randomIndex].text = currentName;
 
     }
+
 }
