b2e01205d5ef3b0381c46441762029456cdf3139
marechem <56137221+marechem@users.noreply.github.com>
2021-03-24 21:38:38 -0500

    Add files via upload
    Adding Menu scripts

---
+++ moonspeak/Assets/Scripts/Animation Scripts/MenuButtonController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	public int index;
+	[SerializeField] bool keyLeft;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis ("Horizontal") != 0){
+			if(!keyLeft){
+				if (Input.GetAxis ("Horizontal") < 0) {
+					if(index < maxIndex){
+						index++;
+					}else{
+						index = 0;
+					}
+				} else if(Input.GetAxis ("Horizontal") > 0){
+					if(index > 0){
+						index --; 
+					}else{
+						index = maxIndex;
+					}
+				}
+				keyLeft = true;
+			}
+		}else{
+			keyLeft = false;
+		}
+	}
+
+}
